c769ea48dad4e460df8ea0ecd99f74ec97440bfc
newstar <kangsaebyeol@gmail.com>
2023-09-07 23:25:07 +0900

    발주서 폼 및 기능 추가

--- app.js
+++ app.js
@@ -2,20 +2,12 @@
 
 $(document).ready(function (e) {
 
-$('#initialize').on('click', function() {
-  $('[contenteditable="true"]').each(function() {
-    $(this).text('');
-  })
-});
-
-  $('[contenteditable="true"]').on('keydown', function() {
-    if ($(this).text() == '') {
-      if (e.keyCode === 8) {
-        // 보류        
-      }
-    }
+  // reset
+  $('#initialize').on('click', function() {
+    $('[contenteditable="true"]').each(function() {
+      $(this).text('');
+    })
   });
-
 
   // HTML2CANVAS API
   const produceImageBtn = document.querySelector('#export');
@@ -53,6 +45,51 @@ $('#initialize').on('click', function() {
       removeCapture();
     }
   });
-})
+
+  
+  function pdfPrint() {
+    window.jsPDF = window.jspdf.jsPDF;
+    $('body').addClass('capture-mode');
+
+    html2canvas(document.querySelector('#capture'), {
+      logging: true,
+      letterRendering: 1,
+      allowTaint: true,
+      useCORS: true,
+    }).then((canvas) => {
+        // 캔버스를 이미지로 변환
+      var imgData = canvas.toDataURL('image/png');
+
+      var imgWidth = 210; // 이미지 가로 길이(mm) A4 기준
+      var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      var doc = new jsPDF('p', 'mm');
+      var position = 0;
+
+      // 첫 페이지 출력
+      doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      // 한 페이지 이상일 경우 루프 돌면서 출력
+      while (heightLeft >= 20) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
+          doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+      }
+
+      // 파일 저장
+      doc.save('paper.pdf');
+      $('body').removeClass('capture-mode');
+      });
+  }
+
+  $('#pdf').on('click', function() {
+    pdfPrint();
+  });
+
+}); // end of ready
 
 
